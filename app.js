cb911c46991ec77dfcb8607a6d3eca0143a05810
Lferg98 <lachieferg98@hotmail.com>
2023-05-08 20:11:36 +1000

    testing api works for now

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next ) => {
-    res.send('Hello World');
+// graphql schemas - resolvers 
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }   
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
+    
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Club event', 'Esoteric Festival', 'My-Aeon'];
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
 })
+);
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
