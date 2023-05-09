60bf84a798ab77ac60a58ec9d756ba75c10aa66b
Lferg98 <lachieferg98@hotmail.com>
2023-05-09 21:49:41 +1000

    build events schema, mutations with gql working with mongodb

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config();
 
 const express = require('express');
@@ -7,14 +6,12 @@ const { buildSchema } = require('graphql');
 
 const { connect, connection } = require('./connection');
 
-
+const Event = require('./models/event');
 
 const app = express();
 const PORT = 3000;
 
 connect();
-
-
 
 const schema = buildSchema(`
   type Event {
@@ -46,8 +43,21 @@ const rootValue = {
     return ['Club event', 'Esoteric Festival', 'My-Aeon'];
   },
   createEvent: (args) => {
-    const eventName = args.name;
-    return eventName;
+    const event = new Event({
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: new Date(args.eventInput.date)
+    });
+    return event.save()
+    .then(result => {
+        console.log(result);
+        return { ...result._doc };
+    })
+    .catch(err => {
+        console.log(err);
+        throw err;
+    });
   },
 };
 
